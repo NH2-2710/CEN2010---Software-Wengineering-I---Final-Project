bfa9bcc63f67c324217ba93d0137b72895b89f17
NH2-2710 <huyhoang687888@gmail.com>
2026-01-27 22:47:49 +0700

    Update FirstAutomationTest.py

--- FirstAutomationTest.py
+++ FirstAutomationTest.py
@@ -23,7 +23,7 @@ class HomeTest(BaseCase):
 
         # click button
         self.click("#VIP_BUNDLE > div.sc-cbd12f50-2.gLMejz > div > picture:nth-child(1) > img")
-        # nhấn nút đóng vào cửa sổ xuất hiện khi khởi động web, tham số truyền vào là css selector
+        # nhấn nút đóng vào cửa sổ quảng cáo xuất hiện khi khởi động web, tham số truyền vào là css selector
         self.sleep(3)# stop the program temporarily for 3 seconds
         self.click("#main-header > div > div > div.sc-b1e0edd7-0.ljZvVE > div.sc-ee984840-0.gypTeU > div.sc-7d80e456-15.dCdTIg > div:nth-child(2)")
         # click the web element of the 
@@ -35,3 +35,5 @@ class HomeTest(BaseCase):
         self.sleep(2) # stop the program temporarily for 2 seconds
         self.scroll_to_bottom() #scroll the web page to bottom
         self.assert_text("Danh Mục Sản Phẩm", "#main-footer > div:nth-child(5) > div > div.sc-8ba33404-0.hrWWOH > div.sc-4ce82f3c-4.aMfcf")
+        # kiểm tra xem web element có chứa chuỗi string không
+
